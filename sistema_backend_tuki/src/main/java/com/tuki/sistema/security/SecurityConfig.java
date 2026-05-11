adca573de22703c906ed5ff8bdbd9a1c212442e3
Jeff-program <jefframi33@gmail.com>
2026-05-10 21:32:48 -0500

    Frontend solucionado

--- sistema_backend_tuki/src/main/java/com/tuki/sistema/security/SecurityConfig.java
+++ sistema_backend_tuki/src/main/java/com/tuki/sistema/security/SecurityConfig.java
@@ -57,7 +57,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:5173", 
+            "http://localhost:3000", 
+            "https://resilient-courage-production-0f33.up.railway.app" // <-- ¡Pon el nuevo aquí!
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
         configuration.setAllowCredentials(true);
